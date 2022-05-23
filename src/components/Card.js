b7a44bdc2cb8c877a7dd029fa215515a18e6d795
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2022-05-23 11:13:24 +0300

    fix: поправить импорты в некоторых файлах

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
